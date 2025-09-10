900b23c5985c9835ac26721369a9e6655c6e3491
Park, Seongjun <pysida70@gmail.com>
2025-09-10 16:54:58 +0900

    Hello 클래스 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -1,8 +1,10 @@
-package git_test2;
-
-public class Main {
-	public static void main(String[] args) {
-		Hello h = new Hello();
-		h.say();
-	}
-}
+package git_test2; 
+ 
+public class Main { 
+ 
+    public static void main(String[] args) { 
+        Hello h = new Hello("hello world"); 
+        h.say(); 
+    } 
+ 
+} 
